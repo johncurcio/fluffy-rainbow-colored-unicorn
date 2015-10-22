e75473339087e24128852ba29f9588edde224430
johncurcio <jfc293@cornell.edu>
2015-10-22 18:49:49 -0400

    Add an actual unicorn

--- unicorn.py
+++ unicorn.py
@@ -1,2 +1,31 @@
 print("What's a unicorn if not colored by rainbow colors?")
 print("A hoarse!")
+unicorn = ''' 
+
+                             \
+                              \
+                               \\
+                                \\
+                                 >\/7
+                             _.-(6'  \
+                            (=___._/` \
+                                 )  \ |
+                                /   / |
+                               /    > /
+                              j    < _\
+                          _.-' :      ``.
+                          \ r=._\        `.
+                         <`\\_  \         .`-.
+                          \ r-7  `-. ._  ' .  `\
+                           \`,      `-.`7  7)   )
+                            \/         \|  \'  / `-._
+                                       ||    .'
+cjr                                     \\  (
+10mar02                                  >\  >
+                                     ,.-' >.'
+                                    <.'_.''
+                                      <'
+
+
+ '''
+print (unicorn)
